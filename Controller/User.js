7719b814ab8910d19cb313816bc34da7dd87b108
Bl4ck-h00d <rohan.kumarjha.cd.mec19@itbhu.ac.in>
2022-07-04 14:20:34 +0530

    [bug]: get profile data author name missing

--- Controller/User.js
+++ Controller/User.js
@@ -7,16 +7,17 @@ class UserController {
 
     //Get posts by user
     const postQuery =
-      "SELECT posts.id, posts.media,posts.edited, posts.title,posts.description,posts.timestamp,posts.tags,posts.ratings, COUNT(comments.comment) AS comments FROM posts LEFT JOIN comments ON posts.id=comments.postId WHERE posts.author=$1 GROUP BY posts.id";
+      "SELECT posts.id,posts.author, posts.media,posts.edited, posts.title,posts.description,posts.timestamp,posts.tags,posts.ratings, COUNT(comments.comment) AS comments FROM posts LEFT JOIN comments ON posts.id=comments.postId WHERE posts.author=$1 GROUP BY posts.id";
 
-    let data = (await pool.query(postQuery, [user])).rows;
+    const data = (await pool.query(postQuery, [user])).rows;
 
+    let responseData = [{ posts: [...data] }];
     const commentQuery =
-      "SELECT comments.id,comments.comment,comments.timestamp,comments.edited,comments.ratings FROM comments WHERE comments.author=$1";
+      "SELECT comments.id,comments.postid, comments.author,comments.comment,comments.timestamp,comments.edited,comments.ratings FROM comments WHERE comments.author=$1";
 
     const comments = (await pool.query(commentQuery, [user])).rows;
 
-    data = [...data, { comments: comments }];
+    responseData = [...responseData, { comments: comments }];
 
     if (
       request.token !== undefined &&
@@ -27,14 +28,13 @@ class UserController {
       const username = token.username;
 
       if (user !== undefined && user !== null && user === username) {
-
         const postsSavedByUserQuery =
-          "SELECT posts.id, posts.media,posts.edited, posts.title,posts.description,posts.timestamp,posts.tags,posts.ratings, COUNT(comments.comment) AS comments FROM posts INNER JOIN savedpost ON posts.id=savedpost.postid LEFT JOIN comments ON posts.id=comments.postId WHERE posts.author=$1 GROUP BY posts.id";
+          "SELECT posts.id,posts.author, posts.media,posts.edited, posts.title,posts.description,posts.timestamp,posts.tags,posts.ratings, COUNT(comments.comment) AS comments FROM posts INNER JOIN savedpost ON posts.id=savedpost.postid LEFT JOIN comments ON posts.id=comments.postId WHERE savedpost.username=$1 GROUP BY posts.id";
 
         const postsSavedByUserData = (
           await pool.query(postsSavedByUserQuery, [username])
         ).rows;
-        data = [...data, { postsSaved: postsSavedByUserData }];
+        responseData = [...responseData, { postsSaved: postsSavedByUserData }];
 
         const anonymousPostsQuery =
           "SELECT posts.id, posts.media,posts.edited, posts.title,posts.description,posts.timestamp,posts.tags,posts.ratings, COUNT(comments.comment) AS comments,anonymousposts.author FROM posts INNER JOIN anonymousposts ON posts.id=anonymousposts.postid LEFT JOIN comments ON posts.id=comments.postId WHERE anonymousposts.author=$1 GROUP BY posts.id,anonymousposts.author";
@@ -42,17 +42,19 @@ class UserController {
         const anonymousPostsByUserData = (
           await pool.query(anonymousPostsQuery, [username])
         ).rows;
-        data = [...data, { anonymousPostsByUser: anonymousPostsByUserData }];
+        responseData = [
+          ...responseData,
+          { anonymousPostsByUser: anonymousPostsByUserData },
+        ];
       }
     }
 
-    return response.status(200).json(data);
+    return response.status(200).json(responseData);
   }
   catch(error) {
     console.log(error);
     return response.status(500).json({ type: "error", msg: error });
   }
-
 }
 
 module.exports = {
